d11fbf1c827833f303de554ddd6fdbca389e9e21
Shashi Kiran <sshashikiransharma@gmail.com>
2023-07-20 21:23:29 +0530

    Initial commit

---
+++ Turtle/main.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+#print(timmy)
+timmy.shape("turtle")
+timmy.color("blue")
+timmy.hideturtle()
+#timmy.shape("arrow")
+timmy.forward(100)
+timmy.penup()
+timmy.backward(200)
+timmy.pendown()
+timmy.left(90)
+timmy.forward(100)
+
+
+myscreen = Screen()
+print(myscreen.canvheight)
+print(myscreen.exitonclick())
