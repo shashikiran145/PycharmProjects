1b7044996474657c2977b571f317bebde183af0c
Shashi Kiran <sshashikiransharma@gmail.com>
2023-07-22 00:07:07 +0530

    1st commit

--- PythonProjects/Turtle/pretty_table.py
+++ PythonProjects/Turtle/pretty_table.py
@@ -3,5 +3,7 @@ table = PrettyTable()
 table.add_column("Pokemon Name", ["Pikachu", "Squirter", "Charmander"])
 table.add_column("Type", ["Electric", "Water", "Fire"])
 table.add_column("Strength", ["1000", "500", "800"])
-table.get_string(sortby="Type")
+#table.get_string(sortby="Type")
+table.align = "l"
+table.sortby = "Pokemon Name"
 print(table)
