c9e5761ef4c6238e3dccc5ebce6c9c81060aef96
Shashi Kiran <sshashikiransharma@gmail.com>
2023-08-10 19:23:20 +0530

    Updated commit

--- PythonProjects/quiz_oop/main.py
+++ PythonProjects/quiz_oop/main.py
@@ -10,8 +10,20 @@ class User:
     def __init__(self, user_id, username):
         self.id = user_id
         self.name = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
 user_1 = User("001", "shashi")
+user_2 = User("002", "kiran")
 
-print(user_1.name)
+user_1.follow(user_2)
+# user_2.follow(user_1)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
