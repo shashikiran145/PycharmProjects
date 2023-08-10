8b3edfdc54705bd765e9fc77db9ff3f553512c91
Shashi Kiran <sshashikiransharma@gmail.com>
2023-08-10 18:44:18 +0530

    Initial commit

--- PythonProjects/quiz_oop/main.py
+++ PythonProjects/quiz_oop/main.py
@@ -1,8 +1,17 @@
+# class User:
+#     pass
+# user_1 = User()
+#
+# user_1.id = "001"
+# user_1.username = "shashi"
+#
+# print(user_1.username)
 class User:
-    pass
-user_1 = User()
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.name = username
 
-user_1.id = "001"
-user_1.username = "shashi"
 
-print(user_1.username)
+user_1 = User("001", "shashi")
+
+print(user_1.name)
