037fda6d13314b0990e5eb052f6a38660355c17d
Shashi Kiran <sshashikiransharma@gmail.com>
2023-08-18 11:59:16 +0530

    Updated code

--- PythonProjects/quiz_oop/quiz-game-start/main.py
+++ PythonProjects/quiz_oop/quiz-game-start/main.py
@@ -11,4 +11,16 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+
+if not quiz.still_has_questions():
+    print("You have completed the quiz")
+    print(f"Your final score is {quiz.score} out of {quiz.question_number}")
+
+
+
+
+
