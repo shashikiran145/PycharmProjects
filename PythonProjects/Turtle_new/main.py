8d55fc3aa6fd534052089ada601d899eb803aae2
Shashi Kiran <sshashikiransharma@gmail.com>
2023-08-31 00:12:50 +0530

    Drawing a square

--- PythonProjects/Turtle_new/main.py
+++ PythonProjects/Turtle_new/main.py
@@ -1,21 +1,14 @@
 from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
-timmy_the_turtle.shape('circle')
+timmy_the_turtle.shape('turtle')
 
+# Drawing a square
+count = 0
 
-
-
-
-
-
-
-
-
-
-
-
-
+for count in range(0, 5):
+    timmy_the_turtle.forward(50)
+    timmy_the_turtle.right(90)
 
 screen = Screen()
 screen.exitonclick()
