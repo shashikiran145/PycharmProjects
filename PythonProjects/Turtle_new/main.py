3faf202b43d76f79fe2265a8defc92f4b03fb555
Shashi Kiran <sshashikiransharma@gmail.com>
2023-08-31 00:45:33 +0530

    Drawing shapes

--- PythonProjects/Turtle_new/main.py
+++ PythonProjects/Turtle_new/main.py
@@ -12,14 +12,29 @@ count = 0
 
 # To draw alternate lines #
 
-tim.penup()
-tim.goto(-200, 0)
+# tim.penup()
+# tim.goto(-200, 0)
+#
+# for count in range(20):
+#     tim.pendown()
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
 
-for count in range(20):
-    tim.pendown()
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
+# Drawing different shapes
+
+angle = 0
+
+for sides in range(4, 10):
+    angle = 360/sides
+    for _ in range(sides):
+        tim.forward(100)
+        tim.left(angle)
+
+
+
+
+
 
 screen = Screen()
 screen.exitonclick()
