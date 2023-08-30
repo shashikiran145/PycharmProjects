7f63db717875312b0c5a9d043f6c3060615c342b
Shashi Kiran <sshashikiransharma@gmail.com>
2023-08-31 00:35:22 +0530

    Drawing a square

--- PythonProjects/Turtle_new/main.py
+++ PythonProjects/Turtle_new/main.py
@@ -3,12 +3,23 @@ from turtle import Turtle, Screen
 tim = Turtle()
 tim.shape('turtle')
 
-# Drawing a square
+# Drawing a square #
 count = 0
 
-for count in range(4):
-    tim.forward(50)
-    tim.right(90)
+# for count in range(4):
+#     tim.forward(50)
+#     tim.right(90)
+
+# To draw alternate lines #
+
+tim.penup()
+tim.goto(-200, 0)
+
+for count in range(20):
+    tim.pendown()
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
 
 screen = Screen()
 screen.exitonclick()
