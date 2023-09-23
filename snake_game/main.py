4aa1cfab0babea469137a133ddcf50262b1510a5
Shashi Kiran <sshashikiransharma@gmail.com>
2023-09-23 23:12:07 +0530

    added code for how the snake dies

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,6 +1,8 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,6 +12,8 @@ screen.tracer(0)
 screen.update()
 
 snake = Snake()
+food = Food()
+score = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -23,8 +27,28 @@ while is_game_on:
     screen.update()
     time.sleep(0.1)
 
+# Detect collision with food
 
-#    snake.move()
+    if snake.head.distance(food) < 20:
+        food.refresh()
+        snake.extend()
+        score.update()
+
+# Detect collision with wall
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        is_game_on = False
+        score.game_over()
+
+# Detect collisions with itself
+
+    for i in snake.segments[1:]:
+        # if i == snake.head:
+        #     pass
+        if snake.head.distance(i) < 10:
+            is_game_on = False
+            score.game_over()
+
 
 screen.exitonclick()
 
