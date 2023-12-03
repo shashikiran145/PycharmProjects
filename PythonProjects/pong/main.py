ac89e0bf44889c4053aab6f600efc6eadb69bb9a
Shashi Kiran <sshashikiransharma@gmail.com>
2023-12-03 23:08:22 +0100

    added ball.py

--- PythonProjects/pong/main.py
+++ PythonProjects/pong/main.py
@@ -1,4 +1,8 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+import time
+
 
 screen = Screen()
 screen.bgcolor('black')
@@ -6,32 +10,45 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape('square')
-paddle.color('white')
-paddle.shapesize(stretch_len=1, stretch_wid=5)
-paddle.penup()
-paddle.goto(350, 0)
 
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
+ball = Ball()
 
 
 screen.listen()
-screen.onkey(go_up, 'Up')
-screen.onkey(go_down, 'Down')
+screen.onkey(r_paddle.go_up, 'Up')
+screen.onkey(r_paddle.go_down, 'Down')
+screen.onkey(l_paddle.go_up, 'w')
+screen.onkey(l_paddle.go_down, 's')
 
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+    # Detect collisions with the screen
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collisions with the paddle. If (== 330) doesn't work, use '>' or '<' sign
+    if ball.distance(r_paddle) < 50 and ball.xcor() == 330 or ball.distance(l_paddle) < 50 and ball.xcor() == -330:
+        ball.bounce_x()
+
+    # Detect if the right paddle misses the ball
+    if ball.xcor() > 400:
+        ball.reset_position()
+
+    # Detect if the left paddle misses the ball
+    if ball.xcor() < -400:
+        ball.reset_position()
+
+
+
+
+
 
 
 
