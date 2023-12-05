829338c78d4c4640fa6a088370239230e5eaaf9f
Shashi Kiran <sshashikiransharma@gmail.com>
2023-12-05 17:22:53 +0100

    new

---
+++ PythonProjects/snake_game_extended/scoreboard.py
@@ -0,0 +1,41 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.high_score = 0
+        self.color('white')
+        self.penup()
+        self.goto(0, 260)
+        self.pendown()
+        self.write(f"Score: {self.score}", False, 'center')
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score}   High Score: {self.high_score}", False, 'center')
+
+    def reset_game(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", False, 'center')
+
+    def increase_scoreboard(self):
+        self.score += 1
+        self.update_scoreboard()
+
+
+
+
+
+
+
