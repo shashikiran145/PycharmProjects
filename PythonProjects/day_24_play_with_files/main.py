f2504b96335df79dd59c1bb35eae4cfb5b065e04
Shashi Kiran <sshashikiransharma@gmail.com>
2023-12-06 12:26:07 +0100

    complete code

--- PythonProjects/day_24_play_with_files/main.py
+++ PythonProjects/day_24_play_with_files/main.py
@@ -8,14 +8,14 @@
 
 # Open and read a file - 2
 
-# with open("my_file.txt") as file:
-#     contents = file.read()
-#     print(contents)
+with open("../../../Desktop/my_file.txt") as file:
+    contents = file.read()
+    print(contents)
 
 # Write something into a file
 # "w" - write ; "a" - append
 
-with open("my_file.txt", mode="a") as file:
-    file.write("\nHello again")
-    print(file.read())
+# with open("my_file.txt", mode="a") as file:
+#     file.write("\nHello again")
+#     print(file.read())
 
