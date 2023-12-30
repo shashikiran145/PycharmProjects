121452eb0cd8a98d56c8bc57d518d399a5c953db
Shashi Kiran <sshashikiransharma@gmail.com>
2023-12-30 11:49:11 +0100

    final commit

--- PythonProjects/day_27_tkinter/main.py
+++ PythonProjects/day_27_tkinter/main.py
@@ -1,11 +1,17 @@
 from tkinter import *
 
+
+def button_clicked():
+    miles = float(ip.get())
+    kms = round((miles * 1.609), 3)
+    print(kms)
+    op["text"] = kms
+
+
 window = Tk()
 window.title("Miles to kms converter")
 window.minsize(width=200, height=200)
 window.config(padx=20, pady=20)    # some space will be left out along the edges
-
-# Create a label
 
 mile = Label(text="miles")
 mile.grid(row=1, column=3)
@@ -13,47 +19,17 @@ mile.grid(row=1, column=3)
 is_equal_to = Label(text="is equal to")
 is_equal_to.grid(row=2, column=1)
 
-op = Label(text="")
+op = Label(text="0")
 op.grid(row=2, column=2)
 
 km = Label(text="kms")
 km.grid(row=2, column=3)
 
-
-# my_label = Label(text="I am a label", font=("Arial", 30, "bold"))
-# pack is required to get the label on the screen
-# my_label.pack()
-# place is used for precise placement, Top left is (0,0)
-# my_label.place(x=10, y=80)
-# Takes input according to grids (rows and columns), but needs a reference for (0,0)
-# my_label.grid(column=0, row=0)
-
-
-# my_label["text"] = "I label"
-# my_label.config(text='I am label')
-#
-
-# Create a button_1
-
-def button_clicked():
-    miles = int(ip.get())
-    kms = round((miles * 1.6), 2)
-    print(kms)
-    op["text"] = kms
-
-
 button_1 = Button(text="Calculate", command=button_clicked)
-# button_1.pack()
-# button_1.place(x=0, y=0)
 button_1.grid(row=3, column=2)
 
-
-# Create an entry
-
 ip = Entry(width=20)
-# input.insert(0, "Enter the number")
-# input.pack()
-# input.place(x=50, y=50)
+ip.insert(0, "0")
 ip.grid(row=1, column=2)
 
 window.mainloop()
