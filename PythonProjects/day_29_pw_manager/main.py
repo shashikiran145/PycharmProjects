4b1f1a5445901f646a2c43126cdc8a3050bcc404
Shashi Kiran <sshashikiransharma@gmail.com>
2024-01-05 12:35:50 +0100

    Initial commit

---
+++ PythonProjects/day_29_pw_manager/main.py
@@ -0,0 +1,51 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    with open("data.txt", mode="a") as data_file:
+        data_file.write(f"{website_entry.get()} | {email_entry.get()} | {password_entry.get()} \n")
+    website_entry.delete(0, END)
+    password_entry.delete(0, END)
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.configure(padx=20, pady=20)
+
+logo_img = PhotoImage(file="logo.png")
+canvas = Canvas(width=200, height=200)
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
+
+
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
+
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "shashi@email.com")
+
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+generate_pw_button = Button(text="Generate password", width=14)
+generate_pw_button.grid(row=3, column=2)
+
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(row=4, column=1, columnspan=2)
+
+
+window.mainloop()
