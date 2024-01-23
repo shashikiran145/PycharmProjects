d2b1dcf29f92ada870d3a9669dfdb6841c4a7827
Shashi Kiran <sshashikiransharma@gmail.com>
2024-01-23 22:53:36 +0100

    commit bro

--- PythonProjects/day_29_pw_manager/main.py
+++ PythonProjects/day_29_pw_manager/main.py
@@ -1,13 +1,53 @@
 from tkinter import *
-
+from tkinter import messagebox
+import random
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+#Password Generator Project
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(numbers) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    password_list = password_numbers + password_letters + password_symbols
+    random.shuffle(password_list)
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
+    # password = ""
+    # for char in password_list:
+    #   password += char
+
+#    print(f"Your password is: {password}")
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    with open("data.txt", mode="a") as data_file:
-        data_file.write(f"{website_entry.get()} | {email_entry.get()} | {password_entry.get()} \n")
-    website_entry.delete(0, END)
-    password_entry.delete(0, END)
+
+    if len(website_entry.get()) == 0 or len(email_entry.get()) == 0 or len(password_entry.get()) == 0:
+        messagebox.showwarning(title="Warning", message="Please don't leave any fields empty")
+
+    else:
+
+        message = messagebox.askokcancel(title="Info", message=f"These are the details entered\n"
+                                                               f"Website: {website_entry.get()}\n"
+                                                               f"Email: {email_entry.get()}\n"
+                                                               f"Are you sure?")
+        if message:
+            with open("data.txt", mode="a") as data_file:
+                data_file.write(f"{website_entry.get()} | {email_entry.get()} | {password_entry.get()} \n")
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -41,7 +81,7 @@ password_label.grid(row=3, column=0)
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
-generate_pw_button = Button(text="Generate password", width=14)
+generate_pw_button = Button(text="Generate password", width=14, command=generate_password)
 generate_pw_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=36, command=save)
---
+++ PythonProjects/day_30_pw_manager_extended/main.py
@@ -0,0 +1,27 @@
+# # Types of error catching codes
+# try:
+#     file = open("a.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["key"])
+# except FileNotFoundError:
+#     file = open("a.txt", "w")
+#     file.write("Hello")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     file.close()
+#     print("File was closed")
+
+
+height = float(input("Enter your height"))
+weight = int(input("Enter your weight"))
+
+if height > 3:
+    raise ValueError("Human height should not exceed 3 meters")
+
+bmi = weight / height ** 2
+
+print(bmi)
